b17ca1a08ef7b12facf46e31a1a0037c54c8d3d2
denoahch <123569094+denoahch@users.noreply.github.com>
2023-03-02 22:25:51 +0100

    Update ShutDownAfterImputTime.bat

--- ShutDownAfterImputTime.bat
+++ ShutDownAfterImputTime.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set /p minutes="Enter the number of minutes after which the PC should be shut down: "
+set /p minutes="Enter the number of seconds after which the PC should be shut down: "
 
-echo The PC will shut down in %minutes% minutes.
+echo The PC will shut down in %minutes% seconds.
 pause
 
-shutdown /s /t %((%minutes%)*60)%
+shutdown /s /t %minutes%
 
